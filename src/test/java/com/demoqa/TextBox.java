e1f02653e91a14e8a1c79c888582b38d6d5690a2
AloeLavi <story_teller@bk.ru>
2022-09-21 21:00:23 +0300

    checking results

--- src/test/java/com/demoqa/TextBox.java
+++ src/test/java/com/demoqa/TextBox.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -52,6 +53,20 @@ public class TextBox {
         $("#city").click();
         $("[id=stateCity-wrapper]").$(byText("Karnal")).click();
         $("#submit").scrollTo().click();
+
+        //results
+        $$(".table-responsive").findBy(text("Student Name")).shouldHave(text("Valeria Bagrova"));
+        $$(".table-responsive").findBy(text("Student Email")).shouldHave(text("vbagrova123456@gmail.com"));
+        $$(".table-responsive").findBy(text("Gender")).shouldHave(text("Female"));
+        $$(".table-responsive").findBy(text("Mobile")).shouldHave(text("1234567890"));
+        $$(".table-responsive").findBy(text("Date of Birth")).shouldHave(text("02 August,1994"));
+        $$(".table-responsive").findBy(text("Subjects")).shouldHave(text("Arts, Maths"));
+        $$(".table-responsive").findBy(text("Hobbies")).shouldHave(text("Reading"));
+        $$(".table-responsive").findBy(text("Picture")).shouldHave(text("crow-of-judgement.jpg"));
+        $$(".table-responsive").findBy(text("Address")).shouldHave(text("Address 123"));
+        $$(".table-responsive").findBy(text("State and City")).shouldHave(text("Haryana Karnal"));
+
+
 
     }
 }
